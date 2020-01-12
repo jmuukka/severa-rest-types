81458725054e40b257b1aad5684101fbb9018037
BLACK\Jarmo <jarmo.muukka@gmail.com>
2020-01-12 13:42:11 +0200

    Initial version 0.1 using Severa version 0.2

---
+++ SeveraApiTypes/SeveraApi.fs
@@ -0,0 +1,9 @@
+﻿namespace Mutex.Visma.Severa
+
+module API =
+
+    open SwaggerProvider
+
+    let [<Literal>] Schema = "https://severa.visma.com/PSARest/restapidoc/docs/v0.2"
+
+    type InternalApi = SwaggerClientProvider<Schema>
